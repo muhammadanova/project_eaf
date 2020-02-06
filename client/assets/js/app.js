16e7cee850473b22384226d8fb7bfe4f28a433ad
Muhammad Anova <anovanurfaqih@gmail.com>
2020-02-07 03:44:37 +0700

    Jquery main setup component

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,66 +1,105 @@
-var baseUrl = 'http://localhost:3000'
+var plan = function() {
+  var baseUrl = 'http://localhost:3000'
 
-// $(window).on("load", function() {
-//   let token = localStorage.getItem("token")
-//   if(token) {
-//     $("#loginPage").hide()
-//     $("#planPage").show()
-//     showAll()
-//   } else {
-//     $("#loginPage").show()
-//     $("#planPage").hide()
-//   }
-// })
+  var loginPage = $("#loginPage")
+  var planList = $("#planList")
+  var loading = $("#loading")
+  var homeSVG = $("#home-svg")
 
-$("#login-form").on('submit', function(e){
-  e.preventDefault()
-  login()
-})
+  var loginForm = $("#login-form")
+  var registForm = $("#register-form")
+  var addPlanForm = $("#addPlanForm")
+  var editPlanForm = $("#editPlanForm")
 
-$("#register-form").on('submit', function(e){
-  e.preventDefault()
-  register()
-})
+  var addTitle = $("#addTitle")
+  var addProvince = $("#addProvince")
+  var addCity = $("#addCity")
+  var addDatePlaen = $("#addDatePlan")
+  var addTransportation = $("#addTransportation")
+  var addBudget = $("#addBudget")
+  var addItinerary = $("#addItinerary")
 
-function login () {
-  $.ajax(`${baseUrl}/login`, {
-    method: "POST",
-    data: {
-      email:$('#email-login').val(),
-      password:$('#password-login').val()
-    },
-    success: function(response){
-      let token = response.token
-      localStorage.setItem("token", token)
-      // $("#loginPage").hide()
-      // showAll()
-      // $("#planPage").show()
-      console.log("berhasil login")
-      Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Successfully login!',
-        showConfirmButton: false,
-        timer: 1500
-      })
-    },
-    error: function(err){
-      console.log(err)
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...', 
-        text: err.responseJSON.msg
-      })
+  var editTitle = $("#editTitle")
+  var editProvince = $("#editProvince")
+  var editCity = $("#editCity")
+  var editDatePlaen = $("#editDatePlan")
+  var editTransportation = $("#editTransportation")
+  var editBudget = $("#editBudget")
+  var editItinerary = $("#editItinerary")
+
+  var emailLogin = $("#email-login")
+  var passLogin = $("#password-login")
+
+  var userRegist = $("#username-regis")
+  var emailRegist = $("#email-regis")
+  var passRegist = $("#password-regis")
+
+  var modalAddPlan = $("#addPlan")
+  var modalDetailPlan = $("#detailPlan")
+  var modalEditPlan = $("#editPlan")
+
+  startSet()
+
+  function startSet(){
+    if(!localStorage.getItem('token')){
+      planList.hide()
+      loading.hide()
+      homeSVG.hide()
+      registForm.hide()
+    }else{
+      
     }
+  }
+
+  loginForm.on('submit', function(e){
+    e.preventDefault()
+    login()
   })
-}
+
+  registForm.on('submit', function(e){
+    e.preventDefault()
+    register()
+  })
+
+  function login () {
+    $.ajax(`${baseUrl}/login`, {
+      method: "POST",
+      data: {
+        email: emailLogin.val(),
+        password: passLogin.val()
+      },
+      success: function(response){
+        let token = response.token
+        localStorage.setItem("token", token)
+        // $("#loginPage").hide()
+        // showAll()
+        // $("#planPage").show()
+        console.log("berhasil login")
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Successfully login!',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      },
+      error: function(err){
+        console.log(err)
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...', 
+          text: err.responseJSON.msg
+        })
+      }
+    })
+  }
 
 function onSignIn(googleUser) {
   var id_token = googleUser.getAuthResponse().id_token;
-$.ajax(`${baseUrl}/google-sign-in`,{
-  method:"POST",
-  data:{
-    id_token
+  $.ajax(`${baseUrl}/google-sign-in`,{
+    method:"POST",
+    data:{
+      id_token
   },
   success: function(response){
     let token = response.token
@@ -108,38 +147,39 @@ function signOut() {
   //     )
   //   }
   // })
- }
+  }
 
- function register(){
-  $.ajax(`${baseUrl}/register`, {
-    method: "POST",
-    data: {
-      username:$('#username-regis').val(),
-      email:$('#email-regis').val(),
-      password:$('#password-regis').val()
-    },
-    success: function(response){
-      console.log("berhasil", response)
-      Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'successfully registered! sign in to start your Plan!',
-        showConfirmButton: false,
-        timer: 3000
-      })
-    },
-    error: function(err){
-      console.log(err)
-      let errors = '<div class="flex-column" style="color:red">'
-      err.responseJSON.errMsg.forEach(salah => {
-        errors += `<div class="col"> ${salah} </div>`
-      })
-      errors += "</div>"
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...', 
-        html: errors
-      })
-    }
-  })
-}
+  function register(){
+    $.ajax(`${baseUrl}/register`, {
+      method: "POST",
+      data: {
+        username: userRegist.val(),
+        email: emailRegist.val(),
+        password: passRegist.val()
+      },
+      success: function(response){
+        console.log("berhasil", response)
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'successfully registered! sign in to start your Plan!',
+          showConfirmButton: false,
+          timer: 3000
+        })
+      },
+      error: function(err){
+        console.log(err)
+        let errors = '<div class="flex-column" style="color:red">'
+        err.responseJSON.errMsg.forEach(salah => {
+          errors += `<div class="col"> ${salah} </div>`
+        })
+        errors += "</div>"
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...', 
+          html: errors
+        })
+      }
+    })
+  }
+}();
